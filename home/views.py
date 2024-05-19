60db099d067f312034891fbdf2944fcfa06860ba
Theo Georgiou <75097082+Theo03iou@users.noreply.github.com>
2024-05-19 22:19:54 +0100

    Fixed Views and links

--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-# from django.http
+from django.http import response
 
 # Create your views here.
 
-def test(self):
-    return render("templates/index.html")
+def index(request):
+    return render(request, "home/index.html")
