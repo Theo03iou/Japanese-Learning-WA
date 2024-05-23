14520a935a13fe7b5360c15c544531503f59f166
Theo Georgiou <75097082+Theo03iou@users.noreply.github.com>
2024-05-23 18:25:40 +0100

    Work for tomorrow

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .forms import LoginForm
+from django.contrib.auth import 
+
 
 urlpatterns = [
-    path("", views.index, name="test"),
+    path('', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login'),
 ]
